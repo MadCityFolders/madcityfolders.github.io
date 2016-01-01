41504c9539b79b40a47bc898e232aed4074e73ef
Liam Marshall <liam@cpan.org>
2016-01-01 13:10:58 -0600

    Port gallery.js away from jquery

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
-  galleryContentsElement = $('#data-gallery-contents');
-  galleryContents = JSON.parse(galleryContentsElement.text());
+  galleryContentsElement = document.getElementById('data-gallery-contents');
+  galleryContents = JSON.parse(galleryContentsElement.textContent);
   console.log(galleryContents);
 
-  gallery = $('#gallery');
+  gallery = document.getElementById('gallery');
   for (var i = 0; i < galleryContents.length; i++) {
     var image = galleryContents[i];
 
-    var imageDiv = $('<div>');
-    var imageLink = $('<a>');
-    var imageElement = $('<img>');
-    imageElement.attr('src', image);
-    imageLink.attr('href', image);
-    imageLink.attr('data-lightbox', 'gallery-image-' + i);
-    imageLink.append(imageElement);
-    imageDiv.append(imageLink);
-    gallery.append(imageDiv);
+    var imageDiv = document.createElement('div');
+    var imageLink = document.createElement('a');
+    var imageElement = document.createElement('img');
+    imageElement.setAttribute('src', image);
+    imageLink.setAttribute('href', image);
+    imageLink.setAttribute('data-lightbox', 'gallery-image-' + i);
+
+    imageLink.appendChild(imageElement);
+    imageDiv.appendChild(imageLink);
+    gallery.appendChild(imageDiv);
   }
 });
